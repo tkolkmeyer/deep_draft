6b23b173388c70c7f85e794562e33d76e6c0ad77
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-03-01 20:00:25 -0600

    first commit

---
+++ src/main/java/com/deepdraft/deep_draft/DeepDraftApplication.java
@@ -0,0 +1,14 @@
+package com.deepdraft.deep_draft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepDraftApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/deepdraft/deep_draft/DeepDraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepdraft.deep_draft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepDraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
