7dea68302a5da553fc81eca03a24fc196ce0a299
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-03-10 16:52:08 -0600

    better than nothing

---
+++ src/main/java/com/deepdraft/deep_draft/models/EndWord.java
@@ -0,0 +1,71 @@
+package com.deepdraft.deep_draft.models;
+
+
+public class EndWord {
+
+    private String EW1;
+    private String EW2;
+    private String EW3;
+    private String EW4;
+    private String EW5;
+    private String EW6;
+
+    public EndWord(String EW1, String EW2, String EW3, String EW4, String EW5, String EW6) {
+        this.EW1 = EW1;
+        this.EW2 = EW2;
+        this.EW3 = EW3;
+        this.EW4 = EW4;
+        this.EW5 = EW5;
+        this.EW6 = EW6;
+    }
+
+    public EndWord() {}
+
+    public String getEW1() {
+        return EW1;
+    }
+
+    public void setEW1(String EW1) {
+        this.EW1 = EW1;
+    }
+
+    public String getEW2() {
+        return EW2;
+    }
+
+    public void setEW2(String EW2) {
+        this.EW2 = EW2;
+    }
+
+    public String getEW3() {
+        return EW3;
+    }
+
+    public void setEW3(String EW3) {
+        this.EW3 = EW3;
+    }
+
+    public String getEW4() {
+        return EW4;
+    }
+
+    public void setEW4(String EW4) {
+        this.EW4 = EW4;
+    }
+
+    public String getEW5() {
+        return EW5;
+    }
+
+    public void setEW5(String EW5) {
+        this.EW5 = EW5;
+    }
+
+    public String getEW6() {
+        return EW6;
+    }
+
+    public void setEW6(String EW6) {
+        this.EW6 = EW6;
+    }
+}
