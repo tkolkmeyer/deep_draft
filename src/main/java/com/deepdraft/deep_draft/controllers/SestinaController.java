28cb71bd3899ecfe036298d6e2ad05097aea0b76
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-03-15 13:27:04 -0500

    template

--- src/main/java/com/deepdraft/deep_draft/controllers/SestinaController.java
+++ src/main/java/com/deepdraft/deep_draft/controllers/SestinaController.java
@@ -15,6 +15,9 @@ public class SestinaController {
     @GetMapping("end_word")
     public String displayEndWordForm(Model model) {
         model.addAttribute("title", "Sestina Formatter");
+        model.addAttribute("sestinaExplanation", "The sestina poetry format is a French tradition that follows a pattern of word repetition. This structure encourages the poet to build a consistent theme or image, and the complicated tradition allows writers to present as obscure academics. But I’m looking to change that last part! \n" +
+                "\n" +
+                "It’s okay if the following diagram is a little strange or confusing. Play around with the sestina tool at your own pace. You’ll get it! ");
         return "sestina/end_word";
     }
 
