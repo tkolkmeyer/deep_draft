d76f282164f21732cd997f3cc5fc32fe4340086b
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-04-23 12:49:51 -0500

    heroku

--- src/main/java/com/deepdraft/deep_draft/DeepDraftApplication.java
+++ src/main/java/com/deepdraft/deep_draft/DeepDraftApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeepDraftApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeepDraftApplication.class, args); 
+		SpringApplication.run(DeepDraftApplication.class, args);
 
 	}
 
